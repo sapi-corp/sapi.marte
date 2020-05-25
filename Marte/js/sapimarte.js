b245466f8448cf4f3701c8b526c061ede319f739
sapi-corp <ivan j@IVASAL>
2020-05-25 16:43:37 -0500

    dia3.2

---
+++ Marte/js/sapimarte.js
@@ -0,0 +1,38 @@
+/*#################     CARGAR PAMATRO   ####################*/
+const realFileBtn = document.getElementById("inpFile");
+const customBtn = document.getElementById("custom-button");
+const customText = document.getElementById("custom-text");
+customBtn.addEventListener("click", function () {
+    realFileBtn.click();
+});
+realFileBtn.addEventListener("change", function () {
+    if (realFileBtn.value) {
+        customText.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+    } else {
+        customText.innerHTML = "no file chose";
+    }
+});
+
+/*################     CARGAR IMAGEN    #####################*/
+const inpFile = document.getElementById("inpFile");
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+
+inpFile.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        previewDefaultText.style.display = "none";
+        previewImage.style.display = "block";
+
+        reader.addEventListener("load", function () {
+            previewImage.setAttribute("src", this.result);
+        });
+        reader.readAsDataURL(file);
+    } else {
+        previewDefaultText.style.display = null;
+        previewImage.style.display = null;
+        previewImage.setAttribute("src", "");
+    }
+});
